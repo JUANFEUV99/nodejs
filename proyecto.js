2342d8224cfa948a2d6db7254f374841d927c025
Juan <jusugav@unal.edu.co>
2018-11-27 13:45:22 -0500

    Actualizado .1

--- proyecto.js
+++ proyecto.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const argv = require("yargs")
-    .command("listar", "imprime en consola la tabla de multiplicar", {
+    .command("listar", "Imprime en consola la tabla de multiplicar", {
         base: {
             demand: true,
             alias: "b"
@@ -10,12 +10,24 @@ const argv = require("yargs")
 
 let cadena = "";
 let base = argv.base;
+let comando = argv._[0];
 
 for (let i = 1; i <= 10; i++) {
     cadena += (`${base} * ${i} = ${base*i}\n`);
 }
 
-fs.writeFile(`tabla${base}.txt`, cadena, err => {
-    if (err) throw err;
-    console.log(`El archivo tabla${base}.txt se creó`);
-});
+switch (comando) {
+    case "listar":
+        console.log(cadena);
+        break;
+    case "crear":
+        fs.writeFile(`tabla${base}.txt`, cadena, err => {
+            if (err) throw err;
+            console.log(`El archivo tabla${base}.txt se creó`);
+        });
+        break;
+
+    default:
+        console.log("Comando no reconocido");
+        break;
+}
